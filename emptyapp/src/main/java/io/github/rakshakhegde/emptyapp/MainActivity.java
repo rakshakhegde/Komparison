abfa5442423b865079155701c19dfd7df14c12f8
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-02-05 15:57:22 +0530

    update readme.md

--- emptyapp/src/main/java/io/github/rakshakhegde/emptyapp/MainActivity.java
+++ emptyapp/src/main/java/io/github/rakshakhegde/emptyapp/MainActivity.java
@@ -1,14 +1,9 @@
 package io.github.rakshakhegde.emptyapp;
 
-import android.databinding.ObservableArrayList;
-import android.support.annotation.Keep;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-	@Keep
-	ObservableArrayList<String> strings;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
